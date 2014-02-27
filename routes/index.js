91333b48d9691a7aa5eda5fd115f5a989c65a409
Elliot Yaghoobia <eyaghoob@ucsd.edu>
2014-02-27 01:37:29 -0800

    analytics lab

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,8 @@ var projects = require('../projects.json');
 exports.view = function(req, res){
   	res.render('index', projects);
 };
+
+exports.viewGrid = function(req, res){
+    projects.grid = true;
+    res.render('index', projects);
+}
